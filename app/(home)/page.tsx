30fe523c16de4d8cff42fcfbc1d495a9aeee6a58
Gustavohsdp <gustavohsdp@gmail.com>
2024-11-15 19:35:40 -0300

    feat: add transactions pie chart component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,8 +2,10 @@ import { auth } from "@clerk/nextjs/server";
 import { isMatch } from "date-fns";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
+import { getDashboard } from "../_data/get-dashboard";
 import { SummaryCards } from "./_components/summary-cards";
 import { TimeSelect } from "./_components/time-select";
+import { TransactionsPieChart } from "./_components/transactions-pie-chart";
 
 interface HomeProps {
   searchParams: {
@@ -24,6 +26,8 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
     redirect("?month=01");
   }
 
+  const dashboard = await getDashboard(month);
+
   return (
     <>
       <Navbar />
@@ -33,7 +37,15 @@ export default async function Home({ searchParams: { month } }: HomeProps) {
           <TimeSelect />
         </div>
 
-        <SummaryCards month={month} />
+        <div className="grid grid-cols-[2fr,1fr]">
+          <div className="flex flex-col gap-6">
+            <SummaryCards {...dashboard} />
+
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+              <TransactionsPieChart {...dashboard} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
