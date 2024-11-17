f533a70abe2582272155735c2d8775566d38ba00
Gustavohsdp <gustavohsdp@gmail.com>
2024-11-16 21:57:58 -0300

    feat: add stripe webhook that handles subscriptions

--- app/subscription/_components/acquire-plan-button.tsx
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { Button } from "@/app/_components/ui/button";
+import { useUser } from "@clerk/nextjs";
 import { loadStripe } from "@stripe/stripe-js";
+import Link from "next/link";
 import { createStripeCheckout } from "../_actions/create-stripe-checkout";
 
 export function AcquirePlanButton() {
+  const { user } = useUser();
+
   async function handleAcquirePlanClick() {
     const { sessionId } = await createStripeCheckout();
 
@@ -21,6 +25,20 @@ export function AcquirePlanButton() {
     }
 
     await stripe.redirectToCheckout({ sessionId });
+  }
+
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan === "premium";
+
+  if (hasPremiumPlan) {
+    return (
+      <Button className="w-full rounded-full font-bold" variant="link">
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user.emailAddresses[0].emailAddress}`}
+        >
+          Gerenciar plano
+        </Link>
+      </Button>
+    );
   }
 
   return (
